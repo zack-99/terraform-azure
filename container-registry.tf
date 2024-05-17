e3eae6abfc28725663a9427d0959ac5034264b59
zack-99 <zacchi99@gmail.com>
2024-05-17 14:21:26 +0200

    initial terraform files

---
+++ container-registry.tf
@@ -0,0 +1,7 @@
+resource "azurerm_container_registry" "main" {
+  name                = "containerRegistry"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "application name" {
+  type = string
+}
+
+variable "environment_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
